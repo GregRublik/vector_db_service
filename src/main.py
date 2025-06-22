8fa263bde98ec29cced3cd56b861b80cd5cd9eda
Иванов Григорий <grigorashekivanov2@gmail.com>
2025-06-22 22:39:13 +0300

    начал работу по внедрению новой архитектуры

---
+++ src/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from config import logger, settings
+import uvicorn
+from api.v1.routers.vectordb import vectordb
+
+app = FastAPI(
+    title="VectorDB API",
+    description="API для работы с векторной базой данных",
+    version="0.1.0"
+)
+
+app.include_router(vectordb, prefix="/api/v1/vectordb")
+
+if __name__ == "__main__":
+    try:
+        uvicorn.run(
+            app,
+            host=settings.app_host,
+            port=settings.app_port,
+            log_config="src/logs/log_config.json",
+            use_colors=True,
+            log_level="info",
+            loop="asyncio"
+        )
+    except Exception as e:
+        logger.error(f"Error launch app: {e}")
